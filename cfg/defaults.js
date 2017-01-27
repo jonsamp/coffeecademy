39f3d4f9319ce3bf057801b5fa7b178d6bed91c1
jonsamp <sampjon@gmail.com>
2017-01-27 14:50:13 -0500

    MVP build

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -55,7 +55,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: '/assets/',
+  publicPath: 'assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
